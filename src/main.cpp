a9775010e80e2388a0aa09b3621b5b6de8211898
Michael Trimarchi <michael@amarulasolutions.com>
2022-08-08 17:03:01 +0200

    main: Update GSETTINGS_SCHEMA_DIR, take in account if already set
    This fix run and debug in gnome-builder and android visual studio code
    
    Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,16 @@
 #include "projectdefinitions.h"
 
 int main(int argc, char** argv) {
-    Glib::setenv("GSETTINGS_SCHEMA_DIR", projectdefinitions::getGeneratedDataDirectory(), false);
+    auto schema_dir = Glib::getenv("GSETTINGS_SCHEMA_DIR");
+
+    if (!schema_dir.empty()) {
+        schema_dir += G_SEARCHPATH_SEPARATOR_S + projectdefinitions::getGeneratedDataDirectory();
+    } else {
+        schema_dir = projectdefinitions::getGeneratedDataDirectory();
+    }
+
+    Glib::setenv("GSETTINGS_SCHEMA_DIR", schema_dir, true);
+
     auto app = Application::create();
     return app->run(argc, argv);
 }
